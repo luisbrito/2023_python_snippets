c2826d9ff45e6e7b8adc6d404a7a6958a7edebde
Luis Fernando Brito <luis.brito-terceiro@cea.com.br>
2023-01-20 15:26:12 -0300

    Update

---
+++ get_news.py
@@ -0,0 +1,65 @@
+import requests
+from sys import argv
+
+API_KEY = 'a1179c70c3f74ee2956d433cd3cd67ea'
+
+URL = ('https://newsapi.org/v2/top-headlines?')
+
+
+def get_artciles_by_category(category):
+    query_parameters = {
+        "category": category,
+        "sortBy": "top",
+        "country": "gb",
+        "apiKey": API_KEY
+    }
+    return _get_articles(query_parameters)
+
+def get_artciles_by_query(query):
+    query_parameters = {
+        "q": query,
+        "sortBy": "top",
+        "country": "gb",
+        "apiKey": API_KEY
+    }
+    return _get_articles(query_parameters)
+
+def _get_articles(params):
+    response = requests.get(URL, params=params)
+
+    articles = response.json()['articles']
+
+    results = []
+        
+    for article in articles:
+        results.append({"title": article["title"], "url": article["url"]})
+
+    for result in results:
+        print(result['title'])
+        print(result['url'])
+        print('')
+
+def get_sources_by_category(category):
+    url = 'https://newsapi.org/v2/top-headlines/sources'
+    query_parameters = {
+        "category": category,
+        "language": "en",
+        "apiKey": API_KEY
+    }
+
+    response = requests.get(url, params=query_parameters)
+
+    sources = response.json()['sources']
+
+    for source in sources:
+        print(source['name'])
+        print(source['url'])
+
+
+if __name__ == "__main__":
+    #print(f"Getting news for {argv[1]}...\n")
+    #get_artciles_by_category(argv[1])
+    #print(f"Successfully retrieved top {argv[1]} headlines")
+    get_artciles_by_category('technology')
+    # get_artciles_by_query("Liz Truss"))
+    #print_sources_by_category("technology")
